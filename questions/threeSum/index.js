304864991663693abdd826a49f7eb498cfac0ac8
BrevenBennett <Breven.stars@gmail.com>
2024-01-06 13:11:19 -0600

    learned how to solve three sum

--- questions/threeSum/index.js
+++ questions/threeSum/index.js
@@ -12,7 +12,35 @@
  */
 
 function threeSum(nums) {
-  
+  nums.sort((a, b) => a - b);
+  const tripsArr = [];
+
+  for (let i = 0; i < nums.length - 2; ++i) {
+    if (nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let l = i + 1;
+    let r = nums.length - 1;
+
+    while (l < r) {
+      const sum = nums[i] + nums[l] + nums[r];
+      if (sum < 0) {
+        l++;
+      }
+      if (sum > 0) {
+        r--;
+      }
+      if (sum === 0) {
+        tripsArr.push([nums[i], nums[l], nums[r]]);
+        l++;
+        while (nums[l] === nums[l - 1] && l < r) {
+          l++;
+        }
+      }
+    }
+  }
+
+  return tripsArr;
 }
 
 //DO NOT EDIT BELOW THIS LINE
