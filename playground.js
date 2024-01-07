6a2ad787c842bfc17f496999cc329e88d38ef8d7
BrevenBennett <Breven.stars@gmail.com>
2024-01-07 14:27:59 -0600

    learned how to solve product except self problem

--- playground.js
+++ playground.js
@@ -1,1 +1,24 @@
-const nums = [1, 2, 4, 9]
+const nums = [1, 2, 3, 4]
+
+const productExceptSelf = (nums) => {
+    const leftProducts = [];
+    const rightProducts = [];
+    const results = [];
+  
+    leftProducts[0] = 1;
+    rightProducts[nums.length - 1] = 1;
+  
+    for (let i = 1; i < nums.length; ++i) {
+      leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
+    }
+  
+    for (let i = nums.length - 1; i > 0; --i) {
+      rightProducts[i] = rightProducts[i + 1] * nums[i + 1];
+    }
+  
+    for (let i = 0; i < nums.length; ++i) {
+      results[i] = leftProducts[i] * rightProducts[i];
+    }
+  
+    console.log(leftProducts);
+  };
--- questions/productExceptSelf/index.js
+++ questions/productExceptSelf/index.js
@@ -2,9 +2,9 @@
  *
  * Write a function that takes in an array of integers 'nums' and
  * returns an array with the product of all integers except the current integer.
- * 
- * Note: You can not use the division operator 
- * 
+ *
+ * Note: You can not use the division operator
+ *
  * Follow up: Can you do this in constant space? The results array does NOT count to the space.
  *
  * @examples
@@ -14,7 +14,22 @@
  */
 
 const productExceptSelf = (nums) => {
+  const results = [];
 
+  results[0] = 1;
+  // creating the left products inside results array
+  for (let i = 1; i < nums.length; ++i) {
+    results[i] = results[i - 1] * nums[i - 1];
+  }
+  // Right product gets bigger as we move from right to left. It's the product of all the numbers to the right of the current number we are on in the loop
+  let rightProduct = 1;
+  for (let i = nums.length - 1; i >= 0; --i) {
+    results[i] = results[i] * rightProduct;
+    // increasing right product as we move through given array
+    rightProduct *= nums[i];
+  }
+
+  return results;
 };
 
 module.exports = productExceptSelf;
