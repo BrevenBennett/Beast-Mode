04b1c0ac3a2a28659aa718c60fe79205b48a203c
BrevenBennett <Breven.stars@gmail.com>
2024-01-08 12:59:38 -0600

    learned how to solve the meeting times problem

--- playground.js
+++ playground.js
@@ -1,24 +1,3 @@
-const nums = [1, 2, 3, 4]
+const nums = [[20, 25], [10, 15]]
 
-const productExceptSelf = (nums) => {
-    const leftProducts = [];
-    const rightProducts = [];
-    const results = [];
-  
-    leftProducts[0] = 1;
-    rightProducts[nums.length - 1] = 1;
-  
-    for (let i = 1; i < nums.length; ++i) {
-      leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
-    }
-  
-    for (let i = nums.length - 1; i > 0; --i) {
-      rightProducts[i] = rightProducts[i + 1] * nums[i + 1];
-    }
-  
-    for (let i = 0; i < nums.length; ++i) {
-      results[i] = leftProducts[i] * rightProducts[i];
-    }
-  
-    console.log(leftProducts);
-  };
+console.log(nums.sort())
--- questions/meetingTimes/index.js
+++ questions/meetingTimes/index.js
@@ -1,17 +1,31 @@
 /** Meeting Times **
- * 
+ *
  * Given an array of meeting times, determine if a person can
- * attend all meetings. 
- *  
+ * attend all meetings.
+ *
  * @example
  * meetingTimes([[10, 15], [20, 25]]) -> true
  * meetingTimes([[5, 10], [10, 15], [12, 25], [25, 30]]) -> false
  * meetingTimes([[10, 20], [20, 30], [30, 40]]) -> true
- * 
+ *
  */
 
- const meetingTimes = (times) => {
+const meetingTimes = (times) => {
+  // sorting the given array in ascending order of the subarray's start times (0th index) 
+  times.sort((a, b) => a[0] - b[0]);
+  // looping through array, need to start at one because we are comparing current index to previous index
+  for (let i = 1; i < times.length; ++i) {
+    // looking at the current subarray's first index
+    const currentStartTime = times[i][0];
+    // looking at the previous subarray's last index
+    const prevEndTime = times[i - 1][1];
+    // if the current subarray's start time is less than the previous subarray's end time, then the times are overlapping. The person cannot attend all the meetings, return false
+    if (currentStartTime < prevEndTime) {
+      return false;
+    }
+  }
 
- };
+  return true;
+};
 
 module.exports = meetingTimes;
