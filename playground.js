733db9ebd8d244d08f3912521da52664b5fa6463
BrevenBennett <Breven.stars@gmail.com>
2024-01-08 14:07:59 -0600

    learned the binary search algorithm

--- playground.js
+++ playground.js
@@ -1,3 +1,21 @@
-const nums = [[20, 25], [10, 15]]
+const nums = [1, 2, 4, 9, 12];
+const target = 9;
 
-console.log(nums.sort())
+const binarySearch = (nums, target) => {
+  let l = 0;
+  let r = nums.length - 1; // 4
+
+  while (l < r) {
+    const middle = Math.floor((l + r) / 2); // 2
+
+    if (nums[middle] < target) { // if 4 < 9
+      l = middle + 1; // l = 3
+    } else if (nums[middle] > target) {
+      r = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+};
+
+console.log(binarySearch(nums, target));
