cb2809e843f7c9d2b491bbc1bb513bdb85494b92
BrevenBennett <Breven.stars@gmail.com>
2024-01-08 22:55:35 -0600

    learned how to use recursion to solve the flatten array problem

--- questions/flattenArray/index.js
+++ questions/flattenArray/index.js
@@ -1,34 +1,33 @@
 /** Flatten Array **
- * 
+ *
  * Given an array 'nums' that can have nested arrays inside it,
- * flatten the array. 
- *  
- * 
+ * flatten the array.
+ *
+ *
  * @example
  * flatten([1, 2, 3, [4, 5]]) -> [1, 2, 3, 4, 5]
  * flatten([1, 2, 3, [4, [5]]]) -> [1, 2, 3, 4, 5]
  * flatten([1, [2, 3, [4, [5]]]]) -> [1, 2, 3, 4, 5]
- * 
+ *
  */
 
- const flatten = (nums) => {
-  const result = []
-
+const flatten = (nums) => {
+  const results = [];
+  // need this helper function so we don't create a new, empty results array every time we recursively call flatten
   const flattenHelper = (nums) => {
-    for (elem of nums){
-      if (Array.isArray(elem)){
-        flattenHelper(elem)
-      }else{
-        result.push(elem)
+    for (elem of nums) {
+      // if the current element is an array, basically start the flatten helper function over. On the second run through the elements of the subarray will then be pushed onto the results
+      if (Array.isArray(elem)) {
+        flattenHelper(elem);
+      } else {
+        results.push(elem);
       }
     }
-  }
+  };
 
-  flattenHelper(nums)
+  flattenHelper(nums);
 
-  return result
+  return results;
+};
 
-
- };
-
-module.exports = flatten; 
+module.exports = flatten;
